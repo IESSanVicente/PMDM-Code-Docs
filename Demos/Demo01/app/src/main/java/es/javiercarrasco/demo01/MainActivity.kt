cb8075b00b554028aa705a422df2f507c0085a87
Javier Carrasco <javier@iessanvicente.com>
2025-10-27 14:53:44 +0100

    Demo 1

---
+++ Demos/Demo01/app/src/main/java/es/javiercarrasco/demo01/MainActivity.kt
@@ -0,0 +1,28 @@
+package es.javiercarrasco.demo01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import es.javiercarrasco.demo01.ui.FormScreen
+import es.javiercarrasco.demo01.ui.theme.Demo01Theme
+
+// MainActivity.kt
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Demo01Theme {
+                FormScreen()
+            }
+        }
+    }
+}
